2d6760902325e3de83dd1f044882fbcdba959747
Michelle Liang <michelle.liang@hackthenorth.com>
2020-12-08 00:20:02 -0500

    robot

--- assets/javascript/notes.js
+++ assets/javascript/notes.js
@@ -1,18 +1,14 @@
 window.onload = function() {
     const acc = document.getElementsByClassName("accordion");
-    console.log(acc);
-    console.log("hi");
     
     for (let i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
-            console.log("Toggling", this.classList)
             /* Toggle between adding and removing the "active" class,
             to highlight the button that controls the panel */
             this.classList.toggle("active");
     
             /* Toggle between hiding and showing the active panel */
             const panel = this.nextElementSibling;
-            console.log(panel)
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
