50a45af9f51bf57d2cb4d6594c1f8ec986de4a42
Michelle Liang <michelle.liang@hackthenorth.com>
2020-12-08 00:13:48 -0500

    js

--- assets/javascript/notes.js
+++ assets/javascript/notes.js
@@ -1,17 +1,19 @@
 const acc = document.getElementsByClassName("accordion");
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    acc[i].addEventListener("click", function() {
+        console.log("Toggling", this.classList)
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    const panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+        /* Toggle between hiding and showing the active panel */
+        const panel = this.nextElementSibling;
+        console.log(panel)
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
