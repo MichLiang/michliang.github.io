3d0df3b8e11f167d5c3c22fdb9b48f07fe7f1e97
theroughcode <raph.koh@gmail.com>
2024-05-12 00:03:21 -0400

    Add rotating moon

---
+++ assets/javascript/index.js
@@ -0,0 +1,12 @@
+const background = document.querySelector(".background");
+
+window.addEventListener("scroll", function () {
+  const scrollPosition = window.pageYOffset;
+  const maxDegreesToRotate = 720;
+  const speed = maxDegreesToRotate / document.documentElement.scrollHeight;
+  const rotation = scrollPosition * speed;
+  const rotationStr = `rotate(${rotation}deg)`;
+  $(".background").css({
+    transform: rotationStr,
+  });
+});
