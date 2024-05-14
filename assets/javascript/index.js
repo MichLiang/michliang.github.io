cb88d6f7a066ee3a77aca75edd340566fcad453e
Michelle Liang <37000837+MichLiang@users.noreply.github.com>
2024-05-14 00:39:30 -0400

    Update index.js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -24,3 +24,9 @@ window.onscroll = function (e) {
   var your_div = document.getElementById('some_div');
   your_div.style.top = (vertical_position + 200) + 'px';//200 is arbitrary.. just to show you could now position it how you want
 }
+
+const demoDiv = document.querySelector("#demo");
+window.addEventListener('scroll', function() {
+  if (pageYOffset*0.0001 > 1 || pageYOffset*0.0001 < 0.2) { return; }
+  else { demo.setAttribute('style', 'transform: scale('+pageYOffset*0.0001+');'); }
+});
