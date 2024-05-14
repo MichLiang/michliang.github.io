39cd9d43cd524b6e39820a22ff40ee64fae2913e
Michelle Liang <37000837+MichLiang@users.noreply.github.com>
2024-05-14 00:26:41 -0400

    Update index.js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -8,5 +8,19 @@ window.addEventListener("scroll", function () {
   const rotationStr = `rotate(${rotation}deg)`;
   $(".moon").css({
     transform: rotationStr,
+    
   });
 });
+
+window.onscroll = function (e) {
+  var vertical_position = 0;
+  if (pageYOffset)//usual
+    vertical_position = pageYOffset;
+  else if (document.documentElement.clientHeight)//ie
+    vertical_position = document.documentElement.scrollTop;
+  else if (document.body)//ie quirks
+    vertical_position = document.body.scrollTop;
+
+  var your_div = document.getElementById('some_div');
+  your_div.style.top = (vertical_position + 200) + 'px';//200 is arbitrary.. just to show you could now position it how you want
+}
